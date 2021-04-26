b6bb53fba11ece6e873cfcf265d5c7e92e1e08fc
Rubén Zagon <ruben.zagon@gmail.com>
2021-04-26 19:31:25 +0100

    disable flyway

--- src/main/resources/db/migration/V0_0_1__initial.sql
+++ src/main/resources/db/migration/V0_0_1__initial.sql
@@ -8,3 +8,7 @@ CREATE TABLE invoices
     AMOUNT       INTEGER
 );
 
+
+INSERT INTO public.invoices (concept, category, amount) VALUES ('guachinche', 'Restaurante', 10);
+INSERT INTO public.invoices (concept, category, amount) VALUES ('netflix', 'Suscripciones', 12);
+INSERT INTO public.invoices (concept, category, amount) VALUES ('xin-xin sushi', 'Restaurante', 30);
